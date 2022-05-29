c99a5b4afe2877423322912264e3d9fb7acfd32d
noobscripter38493 <106062709+noobscripter38493@users.noreply.github.com>
2022-05-28 22:36:10 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -426,7 +426,7 @@ do
     })
     
     local orion = game.CoreGui.Orion
-    local gui_bind = Misc_tab:AddBind({
+    Misc_tab:AddBind({
         Name = "GUI Keybind",
         Default = Enum.KeyCode.RightShift,
         Hold = false,
