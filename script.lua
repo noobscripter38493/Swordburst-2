216bbde73e62caeff96da0c94219b70615baed15
noobscripter38493 <106062709+noobscripter38493@users.noreply.github.com>
2022-05-30 17:11:51 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -442,7 +442,7 @@ do
             end
             
             if math.floor(days) == days then
-                if days == 1 then -- i think days will break the counter but i hope not
+                if days == 1 then -- i think days will break the counter but i hope not (some floating point)
                     displayed = days .. "Day | " .. "%H" .. "Hours | " .. "%M " .. "Minutes | " .. "%S" .. " Seconds"
                 else
                     displayed = days .. "Days | " .. "%H" .. "Hours | " .. "%M " .. "Minutes | " .. "%S" .. " Seconds"
