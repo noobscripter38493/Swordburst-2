87ce486ebb29de34a5313c599528a092788a5a55
noobscripter38493 <106062709+noobscripter38493@users.noreply.github.com>
2022-05-30 21:08:51 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -116,7 +116,7 @@ do
             local attacking = {} -- to overwrite attacking table when toggled off
             if bool then
                 ka = range.Touched:Connect(function(touching)  
-                    if (settings.AttackPlayers and not touched.Parent == char and not touching:FindFirstAncestor("Mobs") and touching.Name == "HumanoidRootPart") or (touching:FindFirstAncestor("Mobs") and touching.Name == "HumanoidRootPart") then
+                    if (settings.AttackPlayers and not touching.Parent == char and not touching:FindFirstAncestor("Mobs") and touching.Name == "HumanoidRootPart") or (touching:FindFirstAncestor("Mobs") and touching.Name == "HumanoidRootPart") then
                         local enemy = touching.Parent
                         
                         if not table.find(attacking, enemy) then -- the touched event will spam - to prevent multiple attacking loops on the same mob
