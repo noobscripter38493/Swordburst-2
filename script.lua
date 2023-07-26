528ad4f72d2653682ee950eb7dbd9ef0398c5895
noobscripter38493 <106062709+noobscripter38493@users.noreply.github.com>
2023-07-26 06:12:44 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1112,7 +1112,7 @@ do
         Name = "Kill Aura",
         Default = false,
         Callback = function(bool)
-            settings.KA = bool
+            settings.KA = false
         end
     })
 
