cbfbece2ed6f350b6b10629712e74bc5651fe919
noobscripter38493 <106062709+noobscripter38493@users.noreply.github.com>
2023-08-23 15:33:10 -0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1796,9 +1796,13 @@ do
 
     local lastcd = 0
     range.Touched:Connect(function(touching)
-        if touching.Parent ~= char and touching.Name ~= "HumanoidRootPart" and not touching:FindFirstAncestor("Mobs") then
+        local enemy = touching.Parent
+        local mob = table.find(mobs_on_floor[placeid], enemy.Name)
+        local boss = table.find(bosses_on_floor[placeid], enemy.Name)
+
+        if not boss and not mob then
             return
-        end 
+        end
 
         local t = os.time()
         if skills:FindFirstChild("Cursed Enhancement") and t - lastcd >= 18 and autoce and stamina.Value >= 30 then
@@ -2742,6 +2746,76 @@ do
                 hiteffects.Parent = nil
             else
                 hiteffects.Parent = workspace
+            end
+        end
+    })
+
+    local nostamfx
+    local noparticles
+    workspace.DescendantAdded:Connect(function(c)
+        if c.Name == "StaminaRegenEffect" and nostamfx then
+            c.Parent = nil
+
+        elseif c:IsA("ParticleEmitter") and noparticles then
+            c.Lifetime = NumberRange.new(0)
+        end
+    end)
+
+    Performance_tab:AddToggle({
+        Name = "No stam regen fx",
+        Default = false,
+        Callback = function(bool)
+            nostamfx = bool
+        end
+    })
+
+    Performance_tab:AddToggle({
+        Name = "No Particles",
+        Default = false,
+        Callback = function(bool)
+            noparticles = bool
+        end
+    })
+
+    Performance_tab:AddButton({
+        Name = 'delete all textures',
+        Default = false,
+        Callback = function()
+            local decalsyeeted = true -- Leaving this on makes games look shitty but the fps goes up by at least 20.
+            local g = game
+            local w = g.Workspace
+            local l = g.Lighting
+            local t = w.Terrain
+            t.WaterWaveSize = 0
+            t.WaterWaveSpeed = 0
+            t.WaterReflectance = 0
+            t.WaterTransparency = 0
+            l.GlobalShadows = false
+            l.FogEnd = 9e9
+            l.Brightness = 0
+            for i, v in pairs(g:GetDescendants()) do
+                if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+                    v.Material = "Plastic"
+                    v.Reflectance = 0
+                elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
+                    v.Transparency = 1
+                elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+                    v.Lifetime = NumberRange.new(0)
+                elseif v:IsA("Explosion") then
+                    v.BlastPressure = 1
+                    v.BlastRadius = 1
+                elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+                    v.Enabled = false
+                elseif v:IsA("MeshPart") then
+                    v.Material = "Plastic"
+                    v.Reflectance = 0
+                    v.TextureID = 10385902758728957
+                end
+            end
+            for i, e in pairs(l:GetChildren()) do
+                if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+                    e.Enabled = false
+                end
             end
         end
     })
